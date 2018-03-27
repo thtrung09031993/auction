2a07d1140fed105bfc3558e507f1cb1d33581d02
Thai Hieu Trung <thtrung09031993@gmail.com>
2018-03-27 11:26:38 +0700

    client main component

---
+++ client/src/ui/MainPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        
+        <div className="container">
+          Hello World
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
